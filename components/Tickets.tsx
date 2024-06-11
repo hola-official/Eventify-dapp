3665c1db23616bfc4c2b3a911454175c12ceffcd
muhammedola <141998553+muhammedola@users.noreply.github.com>
2024-06-11 21:00:31 +0100

    update

--- components/Tickets.tsx
+++ components/Tickets.tsx
@@ -9,7 +9,7 @@ const Ticket: React.FC<{ tickets: TicketStruct[] }> = ({ tickets }) => {
   return (
     <main
       className="lg:w-1/3 w-full mx-auto flex justify-start items-center
-    flex-col sm:space-x-3 bg-white shadow-md overflow-hidden"
+    flex-col sm:space-x-3 bg-[#fefae0] shadow-md overflow-hidden"
     >
       <h4 className="text-xl mt-10 mb-5">All Purchase ({tickets.length})</h4>
       {tickets.map((ticket, i) => (
