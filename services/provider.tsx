db3db42f902845cf88ba41b9f343daf3e0590597
muhammedola <141998553+muhammedola@users.noreply.github.com>
2024-06-11 18:26:04 +0100

    first commit

---
+++ services/provider.tsx
@@ -0,0 +1,102 @@
+'use client'
+
+import * as React from 'react'
+import {
+  GetSiweMessageOptions,
+  RainbowKitSiweNextAuthProvider,
+} from '@rainbow-me/rainbowkit-siwe-next-auth'
+import { WagmiConfig, configureChains, createConfig } from 'wagmi'
+import { Chain, RainbowKitProvider, connectorsForWallets, darkTheme } from '@rainbow-me/rainbowkit'
+import {
+  metaMaskWallet,
+  trustWallet,
+  coinbaseWallet,
+  rainbowWallet,
+} from '@rainbow-me/rainbowkit/wallets'
+import { mainnet, hardhat } from 'wagmi/chains'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+import { Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
+
+const celoAlfajores: Chain = {
+  id: 44787,
+  name: 'Celo Alfajores',
+  network: 'alfajores',
+  iconUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU2dk8Lfcj3vosaen0cFXHBCLPMULaNahY_w&s',
+  iconBackground: '#35D07F',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Celo Alfajores',
+    symbol: 'alfajores',
+  },
+  rpcUrls: {
+    public: { http: ['https://alfajores-forno.celo-testnet.org'] },
+    default: { http: ['https://alfajores-forno.celo-testnet.org'] },
+  },
+  blockExplorers: {
+    default: { name: 'Celo Explorer', url: 'https://explorer.celo.org/alfajores' },
+    etherscan: { name: 'Celo Explorer', url: 'https://explorer.celo.org/alfajores' },
+  },
+  testnet: true,
+}
+
+const { chains, publicClient } = configureChains(
+  [mainnet, celoAlfajores, hardhat],
+  [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID as string }), publicProvider()]
+)
+
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID as string
+
+const connectors = connectorsForWallets([
+  {
+    groupName: 'Recommended',
+    wallets: [
+      metaMaskWallet({ projectId, chains }),
+      trustWallet({ projectId, chains }),
+      coinbaseWallet({ appName: 'Coinbase', chains }),
+      rainbowWallet({ projectId, chains }),
+    ],
+  },
+])
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+})
+
+const demoAppInfo = {
+  appName: 'Dapp Funds dApp',
+}
+
+const getSiweMessageOptions: GetSiweMessageOptions = () => ({
+  statement: `
+  Once you're signed in, you'll be able to access all of our dApp's features.
+  Thank you for partnering with CrowdFunding!`,
+})
+
+export function Providers({
+  children,
+  pageProps,
+}: {
+  children: React.ReactNode
+  pageProps: {
+    session: Session
+  }
+}) {
+  const [mounted, setMounted] = React.useState(false)
+  React.useEffect(() => setMounted(true), [])
+
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <SessionProvider refetchInterval={0} session={pageProps.session}>
+        <RainbowKitSiweNextAuthProvider getSiweMessageOptions={getSiweMessageOptions}>
+          <RainbowKitProvider theme={darkTheme()} chains={chains} appInfo={demoAppInfo}>
+            {mounted && children}
+          </RainbowKitProvider>
+        </RainbowKitSiweNextAuthProvider>
+      </SessionProvider>
+    </WagmiConfig>
+  )
+}
